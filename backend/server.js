4d16dd27398921ed66be9990110761202d711ed3
Devil_Alert <devidasdeo9@gmail.com>
2025-02-01 11:43:17 +0530

    DB connected
    Connected the mongoDB database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import {connectDB} from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
 app.listen(4500, () => {
-  console.log('Server is listening on port 4500...');
+    connectDB();
+    console.log('Server is listening on port 4500...');
 });
